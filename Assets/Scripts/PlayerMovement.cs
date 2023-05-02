7ddc9c606dfba27952d9f79f7fe8c14dc7083b7e
gingalla <ginni.gallagher@gmail.com>
2023-05-01 17:05:56 -0700

    Added player object and implemented simple player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    public CharacterController controller;
+    public float speed = 8f;
+
+    // Start is called before the first frame update
+/*    void Start()
+    {
+        // Might need to place player in scene depending on placement of other assets
+    }
+*/
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
