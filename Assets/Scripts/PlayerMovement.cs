94cfddae058809a20ea4e2d670afdf7dd01a4357
Raul Patel <raulp@uoregon.edu>
2023-05-03 09:38:23 -0700

    Added ambient music and footsteps audio to the game.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,13 +7,35 @@ public class PlayerMovement : MonoBehaviour
     
     public CharacterController controller;
     public float speed = 8f;
+    AudioSource m_AudioSource;
 
     // Start is called before the first frame update
-/*    void Start()
+    void Start()
     {
         // Might need to place player in scene depending on placement of other assets
+        m_AudioSource = GetComponent<AudioSource>();
     }
-*/
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis ("Horizontal");
+        float vertical = Input.GetAxis ("Vertical");
+        bool hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        
+        if (isWalking)
+        {
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
+        }
+        else
+        {
+            m_AudioSource.Stop ();
+        }
+    }
 
     // Update is called once per frame
     void Update()
