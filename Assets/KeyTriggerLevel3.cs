17839eb8731bc28e24442cfcf3fe2a54a3ba971c
gingalla <ginni.gallagher@gmail.com>
2023-05-31 00:29:18 -0700

    Added key pickup for Level 3

---
+++ Assets/KeyTriggerLevel3.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyTriggerLevel3 : MonoBehaviour
+{
+  
+    //public Transform player;
+    public GameObject FloatingText;
+    public GameObject key;
+
+    bool playerInRange;
+    bool textDisplayed = false;
+
+    private GameObject text;
+
+    void OnTriggerEnter(Collider other)
+     {
+ 
+         // Checks to see if tagged player GameObject enters trigger area around the nightstand
+         if (other.gameObject.tag == "Player")
+         {
+             playerInRange = true;
+             Debug.Log("Player in range");
+         }
+     }
+ 
+    void Update()
+     {
+         // if player is in trigger range and text hasn't yet been displayed,
+         // call to display Pick Up Object text
+         if (playerInRange && !textDisplayed)
+         {
+            DisplayFloatingText();
+         }
+
+         if (playerInRange && textDisplayed)
+         {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                KeyPickUp();
+                Destroy(text);
+            }
+         }
+ 
+     }
+ 
+
+    void DisplayFloatingText()
+    {
+        // turns off call in update to display text; otherwise this function is called indefinitely
+        textDisplayed = true;
+
+        Vector3 offset = new Vector3 (1f, 0.5f, 0f);
+
+        // instantiate the prefab
+        // instantiation has specific rotation and position for the key in this scene -- will have to fix for other levels
+        text = Instantiate(FloatingText, key.transform.position + offset, transform.rotation * Quaternion.Euler (180f, -60f, 180f));
+
+        // The font must be large to be rendered clearly. This call shrinks text to necessary size in scene
+        text.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+    }
+
+    void KeyPickUp()
+    {
+        Destroy(key);
+    }
+
+}
