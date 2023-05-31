6155cc54c2270c1066c2f9ce02ec92553a082d4b
clairelouviere <82005932+clairelouviere@users.noreply.github.com>
2023-05-31 11:29:20 -0700

    Clock mostly functional

--- Assets/Clock/Scripts/Clock.cs
+++ Assets/Clock/Scripts/Clock.cs
@@ -5,9 +5,10 @@ public class Clock : MonoBehaviour {
 
 	//-- set start time 00:00
   public Transform player;
-  public Transform FloatingText;
+  //public Transform FloatingText;
   public GameObject clock;
   bool playerInRange;
+  public GameObject FloatingText;
   bool textDisplayed = false;
   private GameObject text;
 	//public bool realTime=true;
@@ -16,13 +17,18 @@ public class Clock : MonoBehaviour {
   public int seconds = 0;
   public bool realTime = false;
 
+  //public int targetHour = 12;
+  public bool puzzleStatus; // has the player has set the time correctly??
+  public GameObject HintText;
+  private GameObject hint;
+  bool hintDisplayed = false;
+
 	public GameObject pointerSeconds;
   public GameObject pointerMinutes;
   public GameObject pointerHours;
 
   //-- time speed factor
-  //public float clockSpeed = 1.0f;     // 1.0f = realtime, < 1.0f = slower, > 1.0f = faster
-  public float clockSpeed = 2.0f;
+  public float clockSpeed = 1.0f;     // 1.0f = realtime, < 1.0f = slower, > 1.0f = faster
   //-- internal vars
   float msecs=0;
 
@@ -40,23 +46,8 @@ public class Clock : MonoBehaviour {
       hour = 9;
       minutes = 0;
       seconds = 0;
-
-      if(playerInRange)
-      {
-        if(!textDisplayed)
-        {
-          //DisplayText();
-        } else {
-          if(Input.GetKeyDown(KeyCode.R))
-          {
-            AddTime();
-          }
-          //if(Input.GetKeyDown(KeyCode.T))
-          //{
-          //  SubTime();
-          //}
-        }
-      }
+      playerInRange = false;
+      puzzleStatus = false; //ie, puzzle not yet solved
   }
   void OnTriggerEnter(Collider other)
   {
@@ -70,15 +61,19 @@ public class Clock : MonoBehaviour {
   {
     if(playerInRange)
     {
-      //if(!textDisplayed)
-      //DisplayText();
+      if(!textDisplayed)
+      {
+        DisplayText();
+      }
       if(Input.GetKeyDown(KeyCode.R))
       {
         AddTime();
-        //minutes += 15;
+      }
+      if(Input.GetKeyDown(KeyCode.T))
+      {
+        SubTime();
       }
     }
-
     //-- calculate time
     msecs += Time.deltaTime * clockSpeed;
     if(msecs >= 1.0f)
@@ -99,6 +94,25 @@ public class Clock : MonoBehaviour {
         }
     }
 
+    // now check if the player has set the time correctly, +/-10 minutes
+    // first check if the minute hand is >55 or <05
+    if((minutes) >=55){
+      if((hour == 11)||(hour == 23)){
+        puzzleStatus = true;
+      }
+    } else if(minutes <= 5) {
+      if((hour == 12) || (hour == 24))
+      {
+        puzzleStatus = true;
+      }
+    }
+    if(puzzleStatus)
+    {
+      hintDisplayed = true;
+      DisplayHint();
+      Destroy(text);
+    }
+
     //-- calculate pointer angles
     float rotationSeconds = (360.0f / 60.0f)  * seconds;
     float rotationMinutes = (360.0f / 60.0f)  * minutes;
@@ -113,7 +127,7 @@ public class Clock : MonoBehaviour {
 
   void AddTime()
   {
-      minutes += 15;
+      minutes += 10;
       if(minutes > 60){
         minutes = 0;
         hour++;
@@ -121,14 +135,51 @@ public class Clock : MonoBehaviour {
           hour = 0;
         }
       }
+  }
 
-    /*
-    void DisplayText()
+  void SubTime(){
+    minutes -= 10;
+    if (minutes < 0)
     {
+      minutes = 60 - minutes;
+      hour--;
+      if(hour==0)
+      {
+        hour = 24;
+      }
+    }
+  }
+
+
+
+
+  void DisplayText()
+  {
+    if(!hintDisplayed)
+    {
+
+
       textDisplayed = true;
       Vector3 offset = new Vector3(1f, 0.5f, 0f);
       //text = Instantiate
+      // instantiate the prefab
+      // instantiation has specific rotation and position for the key in this scene -- will have to fix for other levels
+      text = Instantiate(FloatingText, clock.transform.position + offset, transform.rotation * Quaternion.Euler (90f, -60f, 180f));
+
+      // The font must be large to be rendered clearly. This call shrinks text to necessary size in scene
+      text.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
     }
-    */
   }
+
+  void DisplayHint()
+  {
+    if(text)
+    {
+      Destroy(text);
+    }
+    hintDisplayed = true;
+    Vector3 offset = new Vector3(1f, 0.5f, 0f);
+    hint = Instantiate(HintText, clock.transform.position + offset, transform.rotation * Quaternion.Euler (90f, -60f, 180f));
+  }
+
 }
